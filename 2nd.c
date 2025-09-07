f2d279f80eea0135621d6b1631f2a9dc5f8fdcbe
TejasKanyal <tejaskanyal2006@gmail.com>
2025-09-07 15:45:43 +0530

    Add files via upload

---
+++ 2nd.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main(){
+int length;
+int bredth;
+printf("enter the length:");
+scanf("%d", &length);
+printf("enter the bredth:");
+scanf("%d", &bredth);
+printf("the area of rectangle is %d", length*bredth);
+    return 0;
+}
