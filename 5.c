c31d10d672312af6044ad93d383f6ac70af1801a
TejasKanyal <tejaskanyal2006@gmail.com>
2025-09-07 15:49:03 +0530

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+int a = 29;
+float b = 23.8;
+
+a = (int)b;
+printf("the value of b is %d",a);
+
+    return 0;
+}
---
+++ 6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+float a = 15.0;
+float b = 2.0;
+float c = a/b;
+int d = 6.7;
+printf("the value of a is %f , b is %f , c is %f \n", a,b,c);
+printf(" the value of d is %d" , d);
+    return 0;
+}
